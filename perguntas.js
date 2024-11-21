84cb0a76a3ce8ba50010211c2b9bbafd9863e110
wbella01 <isabelacris199902@gmail.com>
2024-11-21 07:42:37 -0300

    Add files via upload
    alura3

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'programação',
+    'o que é python',
+    ' o python é uma linguagem de programação'
+)
+
+criaCartao(
+    'grografia',
+    ' qual capital da frança?',
+    ' a capital da frança é paris'
+)
+criaCartao(
+    'programação',
+    'o que é uma função',
+     'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    ' lingua inglesa',
+    ' como se diz oi em ingles?',
+    'oi em ingles é hi (RAi)'
+)
